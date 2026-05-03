df44bd7c802b6a5d6b01b35b68a66625c22210aa
inspflof <155272834+inspflof@users.noreply.github.com>
2026-05-03 17:34:33 +0200

    ✨ feat(vehicle): getAll & getById

--- src/controllers/vehicle.ts
+++ src/controllers/vehicle.ts
@@ -7,8 +7,35 @@ const vehicle = {
         const { immatriculation } = req.body
         try {
             const vehicle = await vehicleQueries.create(immatriculation)
-            console.log(vehicle)
             return res.status(200).json(vehicle)
+        } catch (err) {
+            if(err instanceof Error) {
+                return res.status(500).json({ message: err.message })
+            }
+            return res.status(500).json({ message: "Unknown error" })
+        }
+    },
+
+    async getAll(req: Request, res: Response) {
+        try {
+            const vehicles = await vehicleQueries.getAll()
+            return res.status(200).json(vehicles)
+        } catch (err) {
+            if(err instanceof Error) {
+                return res.status(500).json({ message: err.message })
+            }
+            return res.status(500).json({ message: "Unknown error" })
+        }
+    },
+
+    async getById(req: Request, res: Response) {
+        const { vehicleId } = req.params
+        try {
+            if(typeof vehicleId === "string") {
+                const vehicle = await vehicleQueries.getById(vehicleId)
+                return res.status(200).json(vehicle)
+            }
+            throw new Error("Necessary id")
         } catch (err) {
             if(err instanceof Error) {
                 return res.status(500).json({ message: err.message })
--- src/routes/vehicle.ts
+++ src/routes/vehicle.ts
@@ -4,5 +4,8 @@ const route = Router()
 import vehicleController from "../controllers/vehicle.js";
 
 route.post("/", vehicleController.create)
+route.get("/", vehicleController.getAll)
+
+route.get("/:vehicleId", vehicleController.getById)
 
 export default route
